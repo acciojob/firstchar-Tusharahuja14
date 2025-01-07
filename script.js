5f55f8a5efafb0e268131b776b11412fd30f8317
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-01-07 16:55:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstChar(text) {
   // your code here
+	text=text.trim();
+	let j=0;
+	for(let i=0;i<text.length;i++)
+		{
+			if(text.charAt(i)==' ')
+			{
+				j=i;
+				break;
+			}
+		}
+	if(text.length>1)
+	{
+		return text.substring(0,j-1);
+	}
+	else
+	{
+		return '';
+	}
 }
 
 // Do not change the code below
